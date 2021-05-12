139d590f9b21f003ebd918499c76e0ed3c164a73
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 01:18:27 -0500

    Agregar archivos de proyecto.

---
+++ JavaLoops l/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaLoops_l
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame un numero ");
+            int var = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(var + "*" + i + ":" + (var * i));
+            }
+        }
+    }
+}
